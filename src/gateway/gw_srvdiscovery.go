46f6a8afc299d7ce70d39107a74260487a511495
Valery Piashchynski <piashchynski_valery@hotmail.com>
2017-12-22 18:20:44 +0300

    gateway dev

--- src/gateway/gw_srvdiscovery.go
+++ src/gateway/gw_srvdiscovery.go
@@ -1,4 +1,4 @@
-package main
+package gateway
 
 import (
 	consulsd "github.com/go-kit/kit/sd/consul"
@@ -50,45 +50,11 @@ func(s *serviceDiscovery) Find(consulAddress, serviceName, tag *string) (address
 		panic(err)
 	}
 
-	addrs := srventry[0].Node.Address + ":" + strconv.Itoa(srventry[0].Service.Port)
+	addrs := "http://" + srventry[0].Node.Address + ":" + strconv.Itoa(srventry[0].Service.Port)
 
 	return addrs, nil
 }
 
-//func ConsClient(consulAddr *string) consulsd.Client {
-//	var client consulsd.Client
-//	{
-//		consulConfig := api.DefaultConfig()
-//		if len(*consulAddr) > 0 {
-//			consulConfig.Address = *consulAddr
-//		}
-//		consulClient, err := api.NewClient(consulConfig)
-//		if err != nil {
-//			os.Exit(1)
-//		}
-//		client = consulsd.NewClient(consulClient)
-//	}
-//	return client
-//}
 
 
-
-
-//func ServiceD(service, tag string) (address string, e error) {
-//
-//	clnt := ConsClient(&consAddr)
-//	srventry, _, err := clnt.Service(service, tag, true, &consulapi.QueryOptions{})
-//
-//	if len(srventry) == 0 && err == nil {
-//		return "", fmt.Errorf("service ( %s ) was not found", service)
-//	}
-//
-//	if err != nil {
-//		return "", err
-//	}
-//
-//	addrs := srventry[0].Node.Address + ":" +strconv.Itoa(srventry[0].Service.Port)
-//
-//	return addrs, nil
-//}
 
